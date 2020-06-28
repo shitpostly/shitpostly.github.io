6f8dbc8992861b6b0220ef262583eeabee0fca2d
Ein Typ <dongkingkong0@gmail.com>
2020-06-28 13:40:11 +0200

    You can't see it, but I'm making a very angry face right now

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,20 @@ document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.install').scrollIntoView({behavior: 'smooth'});
     e.preventDefault();
   });
+
+  let nameContainer = document.querySelector('.top .brand .name');
+  let name = nameContainer.innerText;
+  nameContainer.innerHTML = '';
+  for (ci in name) {
+    nameContainer.innerHTML += `<span class="letter">${name[ci]}</span>`;
+  }
+
+  let letters = nameContainer.getElementsByClassName('letter');
+  for (let li = 0; li < letters.length; li ++) {
+    window.setTimeout(function () {
+      letters[li].style.opacity = 1;
+    }, li * 80 + 200);
+  }
+
+  document.body.classList.add('content-loaded');
 });
